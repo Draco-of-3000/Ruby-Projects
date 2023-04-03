bf210bc21921c91650c91508d57ac389b20bf3eb
Draco <kevin.tenisson@gmail.com>
2023-04-03 05:22:49 -0700

    Code for Caeser Cipher

---
+++ Caesar.rb
@@ -0,0 +1,26 @@
+def caesar_cipher (plaintext, key)
+    if Numeric === plaintext 
+        return plaintext
+    end
+
+    begin
+        Integer(key)
+    rescue ArgumentError
+        puts "Key must be an integer. E.g '4', '50'"
+    end
+
+    result = ''
+    for i in 0..(plaintext.length - 1)
+        if plaintext[i].match?(/[a-z]/)
+            result += ((plaintext[i].ord - 'a'.ord + key) % 26 + 'a'.ord).chr
+        elsif plaintext[i].match?(/[A-Z]/)
+            result += ((plaintext[i].ord - 'A'.ord + key) % 26 + 'A'.ord).chr
+        else
+            result += plaintext[i]
+        end
+    end
+
+    return result
+end
+
+puts caesar_cipher("I love Draco", 5)
