59d99557e12ed2dc1585e6ab56a24bf39cf5d82e
Draco <kevin.tenisson@gmail.com>
2023-04-03 14:28:11 -0700

    Added comments + improved readability

--- Substrings.rb
+++ Substrings.rb
@@ -3,13 +3,17 @@ require 'pry-byebug'
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 
-def substring (word, dictionary)
+def substring (input, dictionary)
     substring_hash = Hash.new(0)
     
-    dictionary.each do |input|
+    # Loop through every word in the dictionary
+    dictionary.each do |word|
+ 
+        # Check if the word is a substring of the input string
+        if input.downcase.include?(word.downcase)
 
-        if word.downcase.include?(input.downcase)
-            substring_hash[input] += 1
+            # If so, add the word and the number of times it appears to the substring hash
+            substring_hash[word] += 1
         end
     end
 
