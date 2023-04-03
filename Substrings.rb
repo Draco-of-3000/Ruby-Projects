30767b2c2484a5743911b46e3cbf02b7795c1923
Draco <kevin.tenisson@gmail.com>
2023-04-03 14:15:31 -0700

    Added code for Substrings

---
+++ Substrings.rb
@@ -0,0 +1,20 @@
+require 'pry-byebug'
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+
+def substring (word, dictionary)
+    substring_hash = Hash.new(0)
+    
+    dictionary.each do |input|
+
+        if word.include?(input)
+            substring_hash[input] += 1
+        end
+    end
+
+    return substring_hash
+    
+end
+
+puts substring("Howdy partner, sit down! How's it going?", dictionary)
