910bc663ebdee37f334dd37a3c7a4a05c0c1e92c
Draco <kevin.tenisson@gmail.com>
2023-04-12 04:00:40 -0700

    Reverted Rubocop changes

--- Caesar.rb
+++ Caesar.rb
@@ -1,27 +1,26 @@
 def caesar_cipher (plaintext, key)
-    # Check if plaintext is Numeric
-    if Numeric === plaintext 
-        return plaintext
-    end
+  # Check if plaintext is Numeric
+  if Numeric === plaintext 
+      return plaintext
+  end
 
-    #C heck if Key is valid (is an integer)
-    if !key.to_s.match?(/\A\d+\z/)
-        error = puts "INVALID KEY! Cannot Encrypt '#{plaintext}'. Key must be an integer. E.g '4', '50'"
-        return error
-    end
+  # Check if Key is valid (is an integer)
+  if !key.to_s.match?(/\A\d+\z/)
+      error = puts "INVALID KEY! Cannot Encrypt '#{plaintext}'. Key must be an integer. E.g '4', '50'"
+      return error
+  end
 
-    #Encrypt plaintext based on key
-    result = ''
-    for i in 0..(plaintext.length - 1)
-        if plaintext[i].match?(/[a-z]/)
-            result += ((plaintext[i].ord - 'a'.ord + key) % 26 + 'a'.ord).chr
-        elsif plaintext[i].match?(/[A-Z]/)
-            result += ((plaintext[i].ord - 'A'.ord + key) % 26 + 'A'.ord).chr
-        else
-            result += plaintext[i]
-        end
-    end
+  #Encrypt plaintext based on key
+  result = ''
+  for i in 0..(plaintext.length - 1)
+      if plaintext[i].match?(/[a-z]/)
+          result += ((plaintext[i].ord - 'a'.ord + key) % 26 + 'a'.ord).chr
+      elsif plaintext[i].match?(/[A-Z]/)
+          result += ((plaintext[i].ord - 'A'.ord + key) % 26 + 'A'.ord).chr
+      else
+          result += plaintext[i]
+      end
+  end
 
-    return result
+  return result
 end
-
