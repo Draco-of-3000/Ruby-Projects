e6546b878fbf099d492e3606deec8c8bf70693af
Draco <kevin.tenisson@gmail.com>
2023-04-12 04:14:02 -0700

    Reverted Rubocop changes

--- Substrings.rb
+++ Substrings.rb
@@ -2,20 +2,18 @@ dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","o
 
 
 def substring (input, dictionary)
-    substring_hash = Hash.new(0)
+  substring_hash = Hash.new(0)
     
-    # Loop through every word in the dictionary
-    dictionary.each do |word|
+  # Loop through every word in the dictionary
+  dictionary.each do |word|
  
-        # Check if the word is a substring of the input string
-        if input.downcase.include?(word.downcase)
+    # Check if the word is a substring of the input string
+    if input.downcase.include?(word.downcase)
 
-            # If so, add the word and the number of times it appears to the substring hash
-            substring_hash[word] += 1
-        end
+      # If so, add the word and the number of times it appears to the substring hash
+      substring_hash[word] += 1
     end
+  end
 
-    return substring_hash
-    
+  substring_hash
 end
-
