2af18f23e5c40e9680c287309b4d48dab68fa467
Draco <kevin.tenisson@gmail.com>
2023-09-11 04:32:21 -0700

    Modified test for key_is_valid? method

--- spec/caesar_spec.rb
+++ spec/caesar_spec.rb
@@ -6,5 +6,10 @@ describe Caesar do
             caesar = Caesar.new("text", 4)
             expect(caesar.key_is_valid?).to be(true)
         end
+
+        it "returns false if the key is not a valid integer" do 
+            caesar = Caesar.new("text", "invalid")
+            expect(caesar.key_is_valid?).to be(false)
+        end
     end
 end
