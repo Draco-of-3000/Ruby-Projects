ea674fe645b79f23c4648a4c747a17b54fac86f2
Draco <kevin.tenisson@gmail.com>
2023-09-11 03:19:45 -0700

    Transitioned to OOP and deleted Numeric check

--- Caesar.rb
+++ Caesar.rb
@@ -1,26 +1,27 @@
-def caesar_cipher (plaintext, key)
-  # Check if plaintext is Numeric
-  if Numeric === plaintext 
-      return plaintext
-  end
-
-  # Check if Key is valid (is an integer)
-  if !key.to_s.match?(/\A\d+\z/)
-      error = puts "INVALID KEY! Cannot Encrypt '#{plaintext}'. Key must be an integer. E.g '4', '50'"
-      return error
-  end
-
-  #Encrypt plaintext based on key
-  result = ''
-  for i in 0..(plaintext.length - 1)
-      if plaintext[i].match?(/[a-z]/)
-          result += ((plaintext[i].ord - 'a'.ord + key) % 26 + 'a'.ord).chr
-      elsif plaintext[i].match?(/[A-Z]/)
-          result += ((plaintext[i].ord - 'A'.ord + key) % 26 + 'A'.ord).chr
-      else
-          result += plaintext[i]
-      end
-  end
-
-  return result
+class Caesar
+    def initialize(plaintext, key)
+        @plaintext = plaintext
+        @key = key
+    end
+      
+        # Check if Key is valid (is an integer)
+        if !key.to_s.match?(/\A\d+\z/)
+            error = puts "INVALID KEY! Cannot Encrypt '#{plaintext}'. Key must be an integer. E.g '4', '50'"
+            return error
+        end
+      
+        #Encrypt plaintext based on key
+        result = ''
+        for i in 0..(plaintext.length - 1)
+            if plaintext[i].match?(/[a-z]/)
+                result += ((plaintext[i].ord - 'a'.ord + key) % 26 + 'a'.ord).chr
+            elsif plaintext[i].match?(/[A-Z]/)
+                result += ((plaintext[i].ord - 'A'.ord + key) % 26 + 'A'.ord).chr
+            else
+                result += plaintext[i]
+            end
+        end
+      
+        return result
+    end
 end
